b32b3fddf4aad91a5fe431548dc000da9c4b4f72
J3T21 <116262930+J3T21@users.noreply.github.com>
2022-12-08 22:33:33 +0000

    Updated PC to include additional instructions
    Co-authored-by: ahumayde <ahumayde@gmail.com>
    Co-authored-by: Kishok-S <ks3221@ic.ac.uk>
    Co-authored-by: YasserHassan03 <yh4021@ic.ac.uk>

--- rtl/Latest/PCtop.sv
+++ rtl/Latest/PCtop.sv
@@ -4,20 +4,24 @@ module PCtop #(
     input logic CLK,
     input logic rst,
     input logic PCSrc,
+    input logic JumpReg,
+    input logic [D_WIDTH-1:0] Result,
     input logic [D_WIDTH-1:0] ImmExt,
     output logic [D_WIDTH-1:0] PC
 );
 
   // Internal Wires
   logic [D_WIDTH-1:0] PCTarget;
-  logic [D_WIDTH-1:0] PCPlus4;
   logic [D_WIDTH-1:0] PCNext;
+  logic [D_WIDTH-1:0] PCPlus4;
+  logic [D_WIDTH-1:0] PCInterm;
 
-  // Combinational Logic
+  // MUX Logic 
   always_comb begin
     PCPlus4  = PC + 4;
     PCTarget = PC + ImmExt;
-    PCNext   = PCSrc ? PCTarget : PCPlus4;
+    PCInterm = PCSrc ? PCTarget : PCPlus4;
+    PCNext   = JumpReg ? Result : PCInterm;
   end
 
   // DFF Module
