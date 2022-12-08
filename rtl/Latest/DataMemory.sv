560159f1c84dba04be77ff66af514d4028afd2f9
YasserHassan03 <116260803+YasserHassan03@users.noreply.github.com>
2022-12-08 16:11:21 +0000

    Added Data Memory to singlecycle CPU
    Co-authored-by: J3T21 <jameset701@gmail.com>
    Co-authored-by: ahumayde <ahumayde@gmail.com>
    Co-authored-by: Kishok-S <ks3221@ic.ac.uk>

--- rtl/Latest/DataMemory.sv
+++ rtl/Latest/DataMemory.sv
@@ -1,33 +1,38 @@
-module DataMemory#(
-    parameter A_WIDTH=20, RD_WIDTH=32, WD_WIDTH=32
-)(
+module DataMemory #(
+    parameter A_WIDTH = 28,
+    RD_WIDTH = 32,
+    WD_WIDTH = 32,
+    D_WIDTH = 8
+) (
     input  logic                CLK,
     input  logic [WD_WIDTH-1:0] WD,
-    input  logic [A_WIDTH-1:0]  A,
+    input  logic [ A_WIDTH-1:0] A,
     input  logic                WE,
     output logic [RD_WIDTH-1:0] RD
-
 );
-logic [A_WIDTH-1:0] fred [2**A_WIDTH-1:0];
+  // RAM Array
+  logic [D_WIDTH-1:0] RAM[2**A_WIDTH-1:0];
 
-initial begin
-    $display ("Loading fred");
-    $readmemh("fred.hex",fred);
-    $display ("Instructions written to fred successfully");
-    // for (int i=0; i<$size(fred);i++)
-    //     $display(fred[i]," ");
-    //     fred[i]=32'b0;
-end
+  // Load RAM from mem file
+  initial begin
+    $display("Loading RAM");
+    $readmemh("DataMemory.mem", RAM);
+    $display("Instructions written to RAM successfully");
+    // for (int i=0; i<$size(DataMemory);i++)
+    //     $display(DataMemory[i]," ");
+    //     DataMemory[i]=32'b0;
+  end
 
-always_latch begin
-    if (WE==1'b1)
-        fred[A]=WD;
-    RD=fred[A];
-end
+  // Assign Output
+  assign RD = {RAM[A], RAM[A+1], RAM[A+2], RAM[A+3]};
 
-always_ff @(posedge CLK) begin
-    // RD<=fred[A];
-    $display("Address", A);
-    $display("ReadData", RD);
-end
+  // Write to RAM
+  always_ff @(posedge CLK) begin
+    if (WE == 1'b1) begin
+      RAM[A]   <= WD[31:24];
+      RAM[A+1] <= WD[23:16];
+      RAM[A+2] <= WD[15:8];
+      RAM[A+3] <= WD[7:0];
+    end
+  end
 endmodule
