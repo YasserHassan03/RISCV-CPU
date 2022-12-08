90622539232c0311c49b4e08d6f7cfbe4f5b52fd
Kishok-S <ks3221@ic.ac.uk>
2022-12-08 17:48:14 +0000

    Updated top files
    Co-authored-by: J3T21 <jameset701@gmail.com>
    Co-authored-by: ahumayde <ahumayde@gmail.com>
    Co-authored-by: YasserHassan03 <yh4021@ic.ac.uk>

--- rtl/Latest/ALUtop.sv
+++ rtl/Latest/ALUtop.sv
@@ -3,63 +3,65 @@ module ALUtop #(
     D_WIDTH = 32
 ) (
     input  logic               CLK,
+    input  logic               trigger,
+    input  logic               Jump,
     input  logic               ALUSrc,
-    input  logic [        2:0] ALUControl,
+    input  logic [        3:0] ALUControl,
     input  logic               RegWrite,
+    input  logic               PCUppSrc,
+    input  logic               ImmUppSrc,
     input  logic [A_WIDTH-1:0] A1,
     input  logic [A_WIDTH-1:0] A2,
     input  logic [A_WIDTH-1:0] A3,
+    input  logic [D_WIDTH-1:0] PC,
     input  logic [D_WIDTH-1:0] ImmExt,
-    input  logic               ResultSrc,
-    input  logic               MemWrite,
+    input  logic [D_WIDTH-1:0] Result,
     output logic               Zero,
+    output logic [D_WIDTH-1:0] ALUResult,
+    output logic [D_WIDTH-1:0] RD2,
     output logic [D_WIDTH-1:0] a0
 );
 
-  // Internal Wires
+  // Internal Signals
   logic [D_WIDTH-1:0] SrcA;
   logic [D_WIDTH-1:0] SrcB;
-  logic [D_WIDTH-1:0] WriteData;
-  logic [D_WIDTH-1:0] ALUResult;
-  logic [D_WIDTH-1:0] ReadData;
-  logic [D_WIDTH-1:0] Result;
+  logic [D_WIDTH-1:0] RD1;
+  logic [D_WIDTH-1:0] ImmRes;
+  logic [D_WIDTH-1:0] WD3;
+
+  //WD3 MUX's
+  always_comb begin
+    ImmRes = ImmUppSrc ? ImmExt : Result;
+    WD3 = Jump ? PC + 4 : ImmRes;
+  end
 
   // Reg File Module
-  RegFile rf (
+  RegisterFile RF (
       .CLK(CLK),
+      .trigger(trigger),
       .WE3(RegWrite),
       .A1 (A1),
       .A2 (A2),
       .A3 (A3),
-      .WD3(Result),
-      .RD1(SrcA),
-      .RD2(WriteData),
+      .WD3(WD3),
+      .RD1(RD1),
+      .RD2(RD2),
       .a0 (a0)
   );
 
-  // MUX 
-  assign SrcB = ALUSrc ? ImmExt : WriteData;
+  // SrcA MUX
+  assign SrcA = PCUppSrc ? PC + 4 : RD1;
+
+  // SrcB MUX 
+  assign SrcB = ALUSrc ? ImmExt : RD2;
 
   // ALU Module
-  ALU alu (
+  ALU ALU (
       .ALUControl(ALUControl),
       .SrcA(SrcA),
       .SrcB(SrcB),
       .ALUResult(ALUResult),
       .Zero(Zero)
   );
-
-  DataMemory dm (
-      .A  (ALUResult),
-      .WD (WriteData),
-      .WE (MemWrite),
-      .RD (ReadData),
-      .CLK(CLK)
-  );
-
-  always_comb begin
-    assign Result = ResultSrc ? ReadData : ALUResult;
-    $display("Result: ", Result);
-  end
 
 endmodule
--- rtl/Latest/cpu.sv
+++ rtl/Latest/cpu.sv
@@ -1,35 +1,47 @@
 module cpu #(
     parameter WIDTH = 32
 ) (
-    input logic CLK,
-    input logic rst,
+    input  logic CLK,
+    input  logic trigger,
+    input  logic rst,
     output logic [WIDTH-1:0] a0
 );
 
-  wire [WIDTH-1:0] PC;
-  wire [WIDTH-1:0] Instr;
-  wire [WIDTH-1:0] ImmExt;
-  wire             RegWrite;
-  wire             ResultSrc;
-  wire             MemWrite;
-  wire [      2:0] ALUControl;
-  wire             ALUSrc;
-  wire [      2:0] ImmSrc;
-  wire             PCSrc;
-  wire             Zero;
+  // Internal Wires   
+  logic [WIDTH-1:0] PC;
+  logic [WIDTH-1:0] Instr;
+  logic [WIDTH-1:0] ImmExt;
+  logic [WIDTH-1:0] ALUResult;
+  logic [WIDTH-1:0] RD2;
+  logic [WIDTH-1:0] Result;
+  logic             RegWrite;
+  logic             ResultSrc;
+  logic             MemWrite;
+  logic [      3:0] ALUControl;
+  logic             ALUSrc;
+  logic [      2:0] ImmSrc;
+  logic             PCSrc;
+  logic             Jump;
+  logic             JumpReg;
+  logic [      2:0] Type;
+  logic             PCUppSrc;
+  logic             ImmUppSrc;
+  logic             Zero;
 
   // PC Module
   PCtop PCT (
       .CLK(CLK),
       .rst(rst),
       .PCSrc(PCSrc),
+      .JumpReg(JumpReg),
+      .Result(Result),
       .ImmExt(ImmExt),
       .PC(PC)
   );
 
-  // Instr Mem Module
-  InstrMem IM (
-      .A (PC[19:0]),
+  // Instr Memory Module
+  InstrMemory IM (
+      .A (PC[27:0]),
       .RD(Instr)
   );
 
@@ -37,46 +49,67 @@ module cpu #(
   ControlUnit CU (
       .op(Instr[6:0]),
       .funct3(Instr[14:12]),
-      .funct7(Instr[31:25]),
+      .funct7(Instr[30]),
       .Zero(Zero),
+      .Jump(Jump),
+      .JumpReg(JumpReg),
       .PCSrc(PCSrc),
       .ResultSrc(ResultSrc),
+      .Type(Type),
       .MemWrite(MemWrite),
       .ALUControl(ALUControl),
       .ALUSrc(ALUSrc),
       .ImmSrc(ImmSrc),
-      .RegWrite(RegWrite)
+      .RegWrite(RegWrite),
+      .PCUppSrc(PCUppSrc),
+      .ImmUppSrc(ImmUppSrc)
   );
 
   // Sign Extend Unit
   SignExtend SE (
-      .Instr (Instr[31:7]),
+      .Imm(Instr[31:7]),
       .ImmSrc(ImmSrc),
       .ImmExt(ImmExt)
   );
 
   // ALU Module
-  ALUtop ALU (
+  ALUtop ALUT (
       .CLK(CLK),
+      .trigger(trigger),
+      .Jump(Jump),
       .ALUSrc(ALUSrc),
       .ALUControl(ALUControl),
       .RegWrite(RegWrite),
+      .PCUppSrc(PCUppSrc),
+      .ImmUppSrc(ImmUppSrc),
       .A1(Instr[19:15]),
       .A2(Instr[24:20]),
       .A3(Instr[11:7]),
+      .PC(PC),
       .ImmExt(ImmExt),
-      .ResultSrc(ResultSrc),
-      .MemWrite(MemWrite),
+      .Result(Result),
       .Zero(Zero),
+      .ALUResult(ALUResult),
+      .RD2(RD2),
       .a0(a0)
   );
 
+  // Memory Module
+  Memorytop MT (
+      .CLK(CLK),
+      .ALUResult(ALUResult),
+      .RD2(RD2),
+      .Type(Type),
+      .MemWrite(MemWrite),
+      .ResultSrc(ResultSrc),
+      .Result(Result)
+  );
+
+  // Dsiplay
   always_comb begin
-    $display("a0: ", a0);
-    $display("RegWrite: ", RegWrite);
-    $display("Write Address:", Instr[11:7]);
-    $display("ResultSrc: ", ResultSrc);
-
+    // $display("Instruction: %h", Instr);
+    // $display("ALUControl: %b", ALUControl);
+    // $display("ALUResult: %h", ALUResult);
+    // $display("a0: %b", a0);
   end
-
 endmodule
