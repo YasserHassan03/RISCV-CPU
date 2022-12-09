9af154a38f7c214f65b6d99d54e3a47248f263a4
ahumayde <ahumayde@gmail.com>
2022-12-09 10:30:40 +0000

    optimized aludecoder
    Co-authored-by: J3T21 <jameset701@gmail.com>
    Co-authored-by: YasserHassan03 <yh4021@ic.ac.uk>
    Co-authored-by: Kishok-S <ks3221@ic.ac.uk>

--- rtl/Latest/ALUDecoder.sv
+++ rtl/Latest/ALUDecoder.sv
@@ -2,59 +2,25 @@ module ALUDecoder (
     input  logic [1:0] ALUOp,
     input  logic       op5,
     input  logic [2:0] funct3,
-    input  logic [6:0] funct7,
-    output logic [2:0] ALUControl
+    input  logic       funct7,
+    output logic [2:0] Type,
+    output logic [3:0] ALUControl
 );
 
   always_comb begin
+    Type = 3'b111;
     case (ALUOp)
-      // Load Store
-      2'b00: ALUControl = 3'b000;
-      // Branch 
-      2'b01:
-      case (funct3)
-        // Branch Equal - beq
-        3'b00:   ALUControl = 3'b001;
-        // Branch Not Equal - bne
-        3'b01:   ALUControl = 3'b001;
-        // Other Branches maybe use slt aluctrl
-        default: ALUControl = 3'b001;
-      endcase
-      // ALU
-      2'b10:
-      case (funct3)
-        // Add Sub - add, sub
-        3'b000:
-        case ({{op5},{funct7[5]}})
-          // Sub - sub
-          2'b11:   ALUControl = 3'b001;
-          // Add - add                                                                                        
-          default: ALUControl = 3'b000;
-        endcase
-        // Shift Left
-        3'b001: ALUControl = 3'b110;
-        // Set Less Than - slt 
-        3'b010: ALUControl = 3'b101;
-        // Set Less Than Unsigned - sltu
-        3'b011: ALUControl = 3'b101;  // CHANGE
-        // XOR
-        3'b100: ALUControl = 3'b100;
-        // Right Shift
-        3'b101:
-        case (funct7[5])
-          1'b0: ALUControl = 3'b111;
-          // NEEDS CHANGING FOR ARITHMETIC SHIFT
-          1'b1: ALUControl = 3'b111;  // CHANGE
-        endcase
-        // Or - or
-        3'b110: ALUControl = 3'b011;
-        // And - and
-        3'b111: ALUControl = 3'b010;
-        // Default 
-        default: ALUControl = 3'b111;
-      endcase
+      // Load Store 
+      2'b00: begin
+        ALUControl = 4'b0000;
+        Type = funct3;
+      end
+      // Branch (bne/beq = 0000, blt/bge = 0010, bltu/bgeu = 0011)
+      2'b01:   ALUControl = funct3[2] ? {{3'b001}, {funct3[1]}} : 4'b0000;
+      // ALU bit 3 only matters for add/sub vs rsl/rsa so we can us funct3[2] to determine it
+      2'b10:   ALUControl = {{funct3[2] ? {funct7} : {funct7 & op5}}, {funct3}};
       // Default
-      default: ALUControl = 3'b111;
+      default: ALUControl = 4'b0000;
     endcase
   end
 
