1f0490065bef037ac11f1ad1ff9c9ecf560ce45c
ahumayde <ahumayde@gmail.com>
2022-12-09 10:21:55 +0000

    fixed jump and branch + renamed var
    Co-authored-by: J3T21 <jameset701@gmail.com>
    Co-authored-by: YasserHassan03 <yh4021@ic.ac.uk>
    Co-authored-by: Kishok-S <ks3221@ic.ac.uk>

--- rtl/Latest/SignExtend.sv
+++ rtl/Latest/SignExtend.sv
@@ -1,21 +1,23 @@
 module SignExtend (
-    input  logic [31:7] Instr,
+    input  logic [31:7] Imm,
     input  logic [ 2:0] ImmSrc,
     output logic [31:0] ImmExt
 );
 
   always_comb
     case (ImmSrc)
-      // Immediate
-      3'b000:  ImmExt = {{20{Instr[31]}}, {Instr[31:20]}};
+      // Signed Immediate
+      3'b000:  ImmExt = {{20{Imm[31]}}, {Imm[31:20]}};
       // Upper Immediate
-      3'b001:  ImmExt = {{Instr[31:12]}, {12{1'b0}}};
+      3'b001:  ImmExt = {{Imm[31:12]}, {12{1'b0}}};
       // Store
-      3'b010:  ImmExt = {{20{Instr[31]}}, {Instr[31:25]}, {Instr[11:7]}};
+      3'b010:  ImmExt = {{20{Imm[31]}}, {Imm[31:25]}, {Imm[11:7]}};
       // Branch
-      3'b011:  ImmExt = {{21{Instr[31]}}, {Instr[7]}, {Instr[30:25]}, {Instr[11:8]}};
+      3'b011:  ImmExt = {{20{Imm[31]}}, {Imm[7]}, {Imm[30:25]}, {Imm[11:8]}, {1'b0}};
       // Jump      
-      3'b100:  ImmExt = {{13{Instr[31]}}, {Instr[19:12]}, {Instr[20]}, {Instr[30:21]}};
+      3'b100:  ImmExt = {{12{Imm[31]}}, {Imm[19:12]}, {Imm[20]}, {Imm[30:21]}, {1'b0}};
+      // Unsigned Imm
+      //    3'b101   ImmExt = {{20{1'b0}},{Imm[31:20]}};
       // Default
       default: ImmExt = {32{1'b0}};
     endcase
