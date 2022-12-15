df835355bea7ed1bcb71de2a5dff81ecf625b92c
YasserHassan03 <116260803+YasserHassan03@users.noreply.github.com>
2022-12-15 17:04:55 +0000

    Update ExeMemff.sv

--- rtl/Latest/ExeMemff.sv
+++ rtl/Latest/ExeMemff.sv
@@ -1,39 +1,37 @@
 module ExeMemff #(
     parameter WIDTH = 32
 ) (
-    input  logic RegWriteE,
-    input  logic [1:0] ResultSrcE,
-    input  logic MemWriteE,
-    input  logic JumpE,
-    input  logic JumpRegE,
-    input  logic [WIDTH-1:0] AluResultE,
-    input  logic [WIDTH-1:0] WriteDataE, //RD2E
-    input  logic [11:7]RdE,
+    input  logic             CLK,
+    input  logic             RegWriteE,
+    input  logic [1:0]       ResultSrcE,
+    input  logic             MemWriteE,
+    input  logic [2:0]       TypeE,
+    input  logic [WIDTH-1:0] ALUResultE,
+    input  logic [WIDTH-1:0] WriteDataE,
+    input  logic [11:7]      RdE,
     input  logic [WIDTH-1:0] ImmExtE,
     input  logic [WIDTH-1:0] PCPlus4E,
-    input  logic CLK,
-    output logic  RegWriteM,
-    output logic  [1:0] ResultSrcM,
-    output logic  MemWriteM,
-    output logic  JumpM,
-    output logic  JumpRegM,
-    output logic  [WIDTH-1:0] AluResultM,
-    output logic  [WIDTH-1:0] WriteDataM, 
-    output logic  [11:7] RdM,
-    output logic  [WIDTH-1:0] ImmExtM,
-    output logic  [WIDTH-1:0] PCPlus4M   
+    output logic             RegWriteM,
+    output logic [1:0]       ResultSrcM,
+    output logic             MemWriteM,
+    output logic [2:0]       TypeM,
+    output logic [WIDTH-1:0] ALUResultM,
+    output logic [WIDTH-1:0] WriteDataM, 
+    output logic [11:7]      RdM,
+    output logic [WIDTH-1:0] ImmExtM,
+    output logic [WIDTH-1:0] PCPlus4M   
 );
+
 always_ff @(posedge CLK) begin
     RegWriteM <= RegWriteE;
-    ResultSrcM <= RegWriteE;
+    ResultSrcM <= ResultSrcE;
     MemWriteM <= MemWriteE;
-    JumpM <= JumpE;
-    JumpRegM <= JumpRegE;
-    AluResultM <= AluResultE;
+    TypeM <= TypeE;
+    ALUResultM <= ALUResultE;
     WriteDataM <= WriteDataE;
     RdM <= RdE;
-    ImmExtM <= ImmExtE
-    PCPlus4M <= PCPlus4E
+    ImmExtM <= ImmExtE;
+    PCPlus4M <= PCPlus4E;
 end
 
 endmodule
