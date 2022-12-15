0451370fa410d4f4874bc9eaaca7267bf25a280e
J3T21 <116262930+J3T21@users.noreply.github.com>
2022-12-15 17:03:16 +0000

    moved internal logic to external

--- rtl/Latest/PCtop.sv
+++ rtl/Latest/PCtop.sv
@@ -5,21 +5,19 @@ module PCtop #(
     input logic rst,
     input logic PCSrc,
     input logic JumpReg,
+    input logic [D_WIDTH-1:0] PCTarget,
     input logic [D_WIDTH-1:0] Result,
-    input logic [D_WIDTH-1:0] ImmExt,
+    output logic [D_WIDTH-1:0] PCPlus4,
     output logic [D_WIDTH-1:0] PC
 );
 
   // Internal Wires
-  logic [D_WIDTH-1:0] PCTarget;
   logic [D_WIDTH-1:0] PCNext;
-  logic [D_WIDTH-1:0] PCPlus4;
   logic [D_WIDTH-1:0] PCInterm;
 
   // MUX Logic 
   always_comb begin
     PCPlus4  = PC + 4;
-    PCTarget = PC + ImmExt;
     PCInterm = PCSrc ? PCTarget : PCPlus4;
     PCNext   = JumpReg ? Result : PCInterm;
   end
